af5b9c7ceb849952262ea3c276ccb0c310a6e564
Jaen <jaennova9@gmail.com>
2025-01-06 17:52:24 -0600

    update database

--- app/database.py
+++ app/database.py
@@ -6,13 +6,9 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = "sqlite:///./repotech.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL, 
-    connect_args={"check_same_thread": False}
-)
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
