b7b41e30c97cb6eae5d80a655a8cce587298c8d8
Jaen <jaennova9@gmail.com>
2025-01-06 00:37:30 -0600

    add sqlite implementation

--- app/database.py
+++ app/database.py
@@ -6,9 +6,13 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "sqlite:///./repotech.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL, 
+    connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
