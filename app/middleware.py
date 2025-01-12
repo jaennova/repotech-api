1987b0426713a0b14ee290cd9f1f7939b0592d61
Jaen <jaennova9@gmail.com>
2025-01-12 14:51:52 -0600

    Update middleware.py

--- app/middleware.py
+++ app/middleware.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def setup_cors_middleware(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=["https://repotech.vercel.app", "http://localhost:4321"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
